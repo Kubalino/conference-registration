6c6d3c11e716fba2b7ae2fccce18ce68fa5dd206
Noronha <carlos.noronha.93@gmail.comm>
2020-08-31 13:22:00 +0100

    added ViewResolver and ConferenceConfig

--- src/main/java/com/pluralsight/conference/controller/RegistrationController.java
+++ src/main/java/com/pluralsight/conference/controller/RegistrationController.java
@@ -5,11 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Map;
 
 @Controller
-public class RegistrationController {
+public class RegistrationController{
 
     @GetMapping("registration")
     public String getRegistration(@ModelAttribute ("registration") Registration registration) {
